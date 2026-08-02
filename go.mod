50c90730b2720776dd9a84c5218b0e5b9f436d7d
kyoya67 <jackma8569@gmail.com>
2026-08-02 11:22:58 +0900

    Refactor database schema and handlers; remove unused files and update module paths

--- go.mod
+++ go.mod
@@ -1,7 +1,9 @@
-module dbsample
+module github.com/kyoya67/myapi
 
 go 1.25.5
 
-require github.com/go-sql-driver/mysql v1.10.0
-
-require filippo.io/edwards25519 v1.2.0 // indirect
+require (
+	filippo.io/edwards25519 v1.2.0 // indirect
+	github.com/go-sql-driver/mysql v1.10.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,50 +1,21 @@
 package main
 
 import (
-	"database/sql"
-	"dbsample/models"
-	"fmt"
 	"log"
+	"net/http"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/kyoya67/myapi/handlers"
 )
 
 func main() {
-	dbUser := "docker"
-	dbPassword := "docker"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-
-	db, err := sql.Open("mysql", dbConn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	articleID := 10
-	const sqlStr = `
-	SELECT title, contents, username, nice, created_at
-	FROM articles
-	WHERE article_id = ?
-	`
-	row := db.QueryRow(sqlStr, articleID)
-	if err := row.Err(); err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var article models.Article
-	var createdTime sql.NullTime
-
-	err = row.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	if createdTime.Valid {
-		article.CreatedAt = createdTime.Time
-	}
-
-	fmt.Printf("%+v\n", article)
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
