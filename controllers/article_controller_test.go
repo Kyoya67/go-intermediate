a611b54ab5318deae3ffdf646dd64939db5b2723
kyoya67 <jackma8569@gmail.com>
2026-08-12 13:23:19 +0900

    feat: add tests for ArticleDetailHandler with various ID inputs

--- controllers/article_controller_test.go
+++ controllers/article_controller_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func TestArticleListHandler(t *testing.T) {
@@ -25,6 +27,34 @@ func TestArticleListHandler(t *testing.T) {
 			res := httptest.NewRecorder()
 
 			articleController.ArticleListHandler(res, req)
+
+			if res.Code != tt.resultCode {
+				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
+			}
+		})
+	}
+}
+
+func TestArticleDetailHandler(t *testing.T) {
+	var tests = []struct {
+		name       string
+		id         string
+		resultCode int
+	}{
+		{name: "number id", id: "1", resultCode: http.StatusOK},
+		{name: "alphabet id", id: "aaa", resultCode: http.StatusBadRequest},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			url := fmt.Sprintf("http://localhost:8080/article/%s", tt.id)
+			req := httptest.NewRequest(http.MethodGet, url, nil)
+
+			res := httptest.NewRecorder()
+
+			r := mux.NewRouter()
+			r.HandleFunc("/article/{id}", articleController.ArticleDetailHandler).Methods(http.MethodGet)
+			r.ServeHTTP(res, req)
 
 			if res.Code != tt.resultCode {
 				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
