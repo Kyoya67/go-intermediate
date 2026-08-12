49310fd691c9fabe881a9c600bf78d4a6a6ec4c1
kyoya67 <jackma8569@gmail.com>
2026-08-12 15:45:57 +0900

    feat: implement response logging middleware to capture status codes

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,10 +5,26 @@ import (
 	"net/http"
 )
 
+type resLoggingWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, statusCode: http.StatusOK}
+}
+
+func (w *resLoggingWriter) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		log.Println(req.RequestURI, req.Method)
 
-		next.ServeHTTP(w, req)
+		rlw := NewResLoggingWriter(w)
+		next.ServeHTTP(rlw, req)
+		log.Println("res:", rlw.statusCode)
 	})
 }
